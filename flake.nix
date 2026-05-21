847d04cc7bdcb7d36eff3c8b364ec7fe7105a169
nyix <rsa17826@email.vccs.edu>
2026-05-21 18:58:57 -0400

    fixed the hashes

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  inputs = {
+    nixpkgs = {
+      url = "github:NixOS/nixpkgs/nixos-unstable";
+    };
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+    };
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        packages = {
+          # The actual package
+          default = pkgs.buildGoModule {
+            pname = "input-manager";
+            version = "2";
+            src = ./.;
+            vendorHash = "sha256-kDpeH6IuVOk71EzMTANbbPk8Q+AxVf7gj5zLaSmmoos=";
+          };
+        };
+        devShells = {
+          # Development environment
+          default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              go
+              gopls
+            ];
+          };
+        };
+      }
+    );
+}
