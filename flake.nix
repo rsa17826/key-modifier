837b506660a16b8a95972155d0507694e80d4094
nyix <rsa17826@email.vccs.edu>
2026-06-10 08:56:30 -0400

    hash is now valid

--- flake.nix
+++ flake.nix
@@ -26,7 +26,7 @@
             pname = "input-manager";
             version = "2";
             src = ./.;
-            vendorHash = "sha256-JufmKCc7pPe8PVcWeWxlrQP6lI1vKmYdUr8OlqR1/vs=";
+            vendorHash = "sha256-uN8/AHruDSS9vndEjTZit07GjR0rZbbZ0HzII4kG6w4=";
           };
         };
         devShells = {
