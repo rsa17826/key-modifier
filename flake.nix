5fefc04b16aefe3891bcf7bf5499da9e3b88de74
nyix <rsa17826@email.vccs.edu>
2026-09-22 18:24:24 -0400

    NO MESSAGE SET

--- flake.nix
+++ flake.nix
@@ -26,7 +26,7 @@
             pname = "key-modifier";
             version = "2";
             src = ./.;
-            vendorHash = "sha256-RGzLv1EUOkPrfiplyB5sMQzX5zOJTrYyOzs0led1BXQ=";
+            vendorHash = "sha256-y2T0McdFcl4XPK4xhowdUkfHrJLFy0eX7IGoCPDSMbs=";
 
             nativeBuildInputs = [ pkgs.installShellFiles ];
 
